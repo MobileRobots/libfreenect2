b5a97254647af8205332f1510a643b06931e4bfa
Reed Hedges <reed.hedges@adept.com>
2015-02-23 18:35:08 -0500

    Added some additional packages required, and set -e at top.

--- depends/install_ubuntu.sh
+++ depends/install_ubuntu.sh
@@ -1,9 +1,11 @@
 #/bin/sh
 
+set -e
+
 cd `dirname $0`
 DEPENDS_DIR=`pwd`
 
 # Ubuntu debian dependencies
-sudo apt-get install libusb-dev libturbojpeg opencl-headers
+sudo apt-get install libusb-dev libturbojpeg opencl-headers libudev-dev libtool autoconf automake libglew-dev
 
 sh ./install_deps.sh
